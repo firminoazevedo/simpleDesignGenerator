a9049fc1816dd994276fe902692aed8db9e69413
Firmino Azevedo <firmino-017@hotmail.com>
2020-05-30 00:04:38 -0300

    mvp init

---
+++ lib/main.dart
@@ -0,0 +1,326 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    ThemeData _darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    accentColor: Colors.orange,
+    primaryColor: Colors.purple,
+  );
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: _darkTheme,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey globalKey = new GlobalKey();
+
+  String headerText = "";
+  String footerText = "";
+  String centerText = "";
+
+  File _image;
+  File _imageFile;
+
+  bool imageSelected = false;
+
+  Random rng = new Random();
+
+  Future getImage() async {
+    var image;
+    try {
+      image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    } catch (platformException) {
+      print("not allowing " + platformException);
+    }
+    setState(() {
+      if (image != null) {
+        imageSelected = true;
+      } else {}
+      _image = image;
+    });
+    new Directory('storage/emulated/0/' + 'MemeGenerator')
+        .create(recursive: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Image.asset(
+                "assets/memegenratorF.png",
+                height: 70,
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              RepaintBoundary(
+                key: globalKey,
+                child: Stack(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 1.0,
+                      child: _image != null
+                          ? Image.file(_image, fit: BoxFit.cover)
+                          : Container(),
+                    ),
+
+                    // image background
+                    _image != null ? AspectRatio(
+                      aspectRatio: 1.0,
+                      child: Image.asset(
+                        "assets/bg.png",
+                      ),
+                    ) : Container(),
+                    
+                    // texto central
+                    /*Positioned(
+                      top: MediaQuery.of(context).size.width / 2,
+                      left: 90,
+                      child: RotationTransition(
+                        turns: AlwaysStoppedAnimation(-8 / 360),
+                        child: Center(
+                          child: AutoSizeText(
+                            centerText.toUpperCase(),
+                            maxFontSize: 28,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                fontSize: 35,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),*/
+
+                    Positioned(
+                      top: MediaQuery.of(context).size.width / 2,
+                      left: 80,
+                      right: 80,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(80),
+                          //color: Colors.white,
+                        ),
+                        child: RotationTransition(
+                          turns: AlwaysStoppedAnimation(-8 / 360) ,
+                            child: Center(
+                            child: AutoSizeText(
+                              centerText.toUpperCase(),
+                              maxLines: 1,
+                              maxFontSize: 28,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 28,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // texto do meio
+                    Positioned(
+                      top: MediaQuery.of(context).size.width / 1.52,
+                      left: 80,
+                      right: 80,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(80),
+                          color: Colors.white,
+                        ),
+                        child: Center(
+                          child: Text(
+                            headerText.toUpperCase(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 17,
+                                color: Color.fromRGBO(11, 56, 40, 1)),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // texto de especialidade
+                    Positioned(
+                      top: MediaQuery.of(context).size.width / 1.37,
+                      left: 90,
+                      right: 90,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                        decoration: BoxDecoration(),
+                        child: Center(
+                          child: Text(
+                            footerText.toUpperCase(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 17,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          //buildText(headerText),
+                          Spacer(),
+                          //buildText(footerText),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+
+              imageSelected
+                  ? Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            onChanged: (val) {
+                              setState(() {
+                                centerText = val;
+                              });
+                            },
+                            decoration:
+                                InputDecoration(
+                                  icon: Icon(Icons.text_fields),
+                                  hintText: "Texto central"),
+                          ),
+                          TextField(
+                            
+                            onChanged: (val) {
+                              setState(() {
+                                headerText = val;
+                              });
+                            },
+                            decoration:
+                                InputDecoration(
+                                  icon: Icon(Icons.account_circle),
+                                  hintText: "Nome do Profissional"),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          TextField(
+                            onChanged: (val) {
+                              setState(() {
+                                footerText = val;
+                              });
+                            },
+                            decoration:
+                                InputDecoration(
+                                  icon: Icon(Icons.mode_edit),
+                                  hintText: "Especialidade"),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          RaisedButton(
+                            onPressed: () {
+                              takeScreenshot();
+                            },
+                            child: Text("Salvar"),
+                          )
+                        ],
+                      ),
+                    )
+                  : Container(
+                      child: Center(
+                        child: Text("Clique no botão adicionar imagem"),
+                      ),
+                    ),
+              _imageFile != null ? Image.file(_imageFile) : Container(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getImage();
+        },
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+  Widget buildText(String text) {
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        child: Text(
+          text.toUpperCase(),
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 26,
+          ),
+        ));
+  }
+
+  
+  takeScreenshot() async {
+    RenderRepaintBoundary boundary = globalKey.currentContext.findRenderObject();
+    ui.Image image = await boundary.toImage( pixelRatio: 2.2);
+    final directory = (await getApplicationDocumentsDirectory()).path;
+    ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData.buffer.asUint8List();
+    //print(pngBytes);
+    File imgFile = File('$directory/screenshot${rng.nextInt(200)}.png');
+    setState(() {
+      _imageFile = imgFile;
+    });
+    _savefile(_imageFile);
+    //saveFileLocal();
+    imgFile.writeAsBytes(pngBytes);
+  }
+
+  _savefile(File file) async {
+    await _askPermission();
+    final result = await ImageGallerySaver.saveImage(
+        Uint8List.fromList(await file.readAsBytes()));
+    print(result);
+  }
+
+  _askPermission() async {
+    Map<PermissionGroup, PermissionStatus> permissions =
+        await PermissionHandler().requestPermissions([PermissionGroup.photos]);
+  }
+}
